0a7097ca4b09511ed628094a45a8bbc98ab13fa0
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-08-24 01:08:25 -0400

    only gets internal links now

--- wriggler.py
+++ wriggler.py
@@ -1,13 +1,14 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+# https://stackoverflow.com/questions/9029822/how-can-i-bring-google-like-recrawling-in-my-applicationweb-or-console/9099798#9099798
 def scrape(root_url, url):
     # do some conditional checking here
     url = root_url + url
     url = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(url, 'html.parser')
     link_tags = soup.find_all('a')
-    links = [link['href'] for link in link_tags if 'href' in link.attrs.keys()]
+    links = [link['href'] for link in link_tags if 'href' in link.attrs.keys() and link['href'].startswith('.')]
     return links
 
 root_url = 'https://vaibhavsagar.com/'
