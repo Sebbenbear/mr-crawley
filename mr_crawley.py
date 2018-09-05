887cbd78ca9c87ac02f4a4029e56b8b2ddff4f16
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-09-04 20:28:22 -0400

    Change datastructure from list to use a defaultdict from parent urls to the list of components: this is an adjacency list.

--- mr_crawley.py
+++ mr_crawley.py
@@ -1,11 +1,14 @@
 from time import time
 
 start = time()
-from collections import deque
+from collections import deque, defaultdict
 import sys
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
+
+import aiohttp
+import asyncio
 
 root_url = sys.argv[1]
 
@@ -20,7 +23,7 @@ def scrape_url(root_url, url):
             result.append(current_link)
     return result
 
-site_map = deque()
+site_map = defaultdict(set)
 visited = set()
 urls = deque([root_url])
 
@@ -28,11 +31,16 @@ while urls:
     url = urls.pop()
     visited.add(url)
     new_urls = scrape_url(root_url, url)
-    site_map.extend(new_urls)
+    for new_url in new_urls:
+        site_map[url].add(new_url)
     urls.extend([url for url in new_urls if url not in visited])
 
+# Finally, print the site map
 for url in site_map:
     print(url)
+    values = site_map[url]
+    for value in values:
+        print('\t' + value)
 
 end = time()
 print('Time taken: {0:.2f}'.format(end-start))
